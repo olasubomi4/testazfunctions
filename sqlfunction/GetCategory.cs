0c5d76da34db36b407aa8408c2f3dc68c8a36c37
Odekunle Olasubomi <40355148+olasubomi4@users.noreply.github.com>
2022-07-10 08:50:27 +0100

    Update GetCategory.cs

--- sqlfunction/GetCategory.cs
+++ sqlfunction/GetCategory.cs
@@ -7,7 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 using System.Collections.Generic;
 
 namespace sqlfunction
